d726a0c94da706b62df500c7183b82f6628644db
Chad Vincent <ccrvincent@gmail.com>
2023-10-28 16:48:16 -0500

    Work to bring the Team Last Donation widget inline with the regular Last Donation widget. (#27)

--- team-last-donation/team-last-donation-settings.js
+++ team-last-donation/team-last-donation-settings.js
@@ -1,16 +1,27 @@
 ELT.settings = {
-	// get donation information for this team
-	teamId: "38901",
-	// if the header message should be shown
-	showHeader: false,
-	// header message to display at the top of the widget
-	headerMessage: "Last Team Donation",
-	// name to show if the donor name is null
-	unknownDonorName: "Mysterious Hero",
-	// text to use between the donation and the participant name
-	conjunctionText: "donated to",
-	// how long to display each donation before going to the next.	
-	donationCycleMS: 10000,
-	// how often should data be refreshed
-	refreshTimeMS: 15000,
+    // animate the last donation instead of persist on screen
+    animate: false,
+    // animation pause duration
+    animationPauseMS: 5000,
+    // direction of animation
+    animateTo: 'left',
+    // get donation information for this team
+    teamId: "64013",
+    // name to show if the donor name is null
+    unknownDonorName: "Mysterious Hero",
+    // Message displayed when donation amount it private to the donatee
+    unknownDonationAmountText: "Private Donation",
+    // if the header message should be shown
+    showHeader: true,
+    // header message to display at the top of the widget
+    headerMessage: "New Donation!",
+    // text to use between the donation and the participant name
+    conjunctionText: "donated to",
+    // if the recipiant of the donation should be shown. This is meant
+    // for when you have more than one participantID
+    showRecipient: true,
+    // how long to display each donation before going to the next.
+    donationCycleMS: 10000,
+    // how often should data be refreshed
+    refreshTimeMS: 15000,
 };
--- team-last-donation/team-last-donation.js
+++ team-last-donation/team-last-donation.js
@@ -1,75 +1,95 @@
-(function( $, ELT ){
+(function ($, ELT) {
     /**********
      * Main Functionality
      **********/
 
-	var teamMembers = {};
-	var newDonations = [];
-	var donationsSeen = 0;
-	var itemsToAdd = 1;
+    var $participants = {};
+    var $newDonations = [];
+    var donationsSeen = 0;
+    var itemsToAdd = 1;
+	// Structure placeholders
+    const $donation = $('#donation');
+    const $participantName = $('#participant-name');
+    const $trackingContainer = $('#tracking-container');
+    const $header = $('#header');
+    const $donationConjunction = $('#donation-conjunction');
 
     /* Initial setup of the layout and theme based on user settings */
     function start() {
-		const header = $('#header');
 
-		if (ELT.settings.showHeader) {
-			header.html(ELT.settings.headerMessage);
-		} else {
-			header.hide();
-		}
+        if (ELT.settings.showHeader) {
+            $header.html(ELT.settings.headerMessage);
+        } else {
+            $header.remove();
+        }
 
-		$('#donation-conjunction').html(ELT.settings.conjunctionText);
+        if (ELT.settings.animate) {
+            $trackingContainer.addClass('animate')
+            // add animation direction
+            .addClass(`animate-${ELT.settings.animateTo}`);
+        }
 
-		ELT.api.teamParticipants(ELT.settings.teamId, function(results) {
-			results.forEach(function(teamMember) {
-				teamMembers[teamMember.participantID] = teamMember;
-			});
-			
-			checkForNewDonations();
-			updateDonation();
-			setInterval(checkForNewDonations, ELT.settings.refreshTimeMS);
-			setInterval(updateDonation, ELT.settings.donationCycleMS); 
-		});   
-	}
+        if (ELT.settings.showRecipient) {
+            $donationConjunction.html(ELT.settings.conjunctionText);
+        } else {
+            $donationConjunction.remove();
+            $participantName.remove();
+        }
+
+        ELT.api.teamParticipants(ELT.settings.teamId, function (results) {
+            results.forEach(function (teamMember) {
+                $participants[teamMember.participantID] = teamMember;
+            });
+        });
+
+        checkForNewDonations();
+        updateDonation();
+        setInterval(checkForNewDonations, ELT.settings.refreshTimeMS);
+        setInterval(updateDonation, ELT.settings.donationCycleMS);
+    }
 
     /* Main loop */
     function checkForNewDonations() {
-		ELT.api.teamDonations(ELT.settings.teamId, checkForNewDonationsOnSuccess);
+        ELT.api.teamDonations(ELT.settings.teamId, checkForNewDonationsOnSuccess);
     }
 
-    function checkForNewDonationsOnSuccess(result) {
-		if (result.length > donationsSeen) {
-			const newItems = result.slice(0, result.length - donationsSeen);
-			
-			newDonations = newDonations.concat(newItems);
-			donationsSeen = result.length;
-		}
-	}
-	
-	function updateDonation() {
-		if (newDonations.length > 0) {
-			const $donation = $('#donation');
-			const $participantName = $('#participant-name');
-			let value;
-			let participant;
-			const lastDonation = newDonations.pop();
+    function checkForNewDonationsOnSuccess(results) {
+        if (results.length > donationsSeen) {
+            const newItems = results.slice(0, results.length - donationsSeen);
 
-			if( lastDonation ){
-				const amount = ELT.toCurrency(lastDonation.amount);
-				const donorName = lastDonation.displayName == null ? 
-					ELT.settings.unknownDonorName : lastDonation.displayName;
-				
-				participant = teamMembers[lastDonation.participantID].displayName;
-				value = `${donorName}: ${amount}`;
-			} else {
-				participant = ' ';
-				value = 'No donations';
-			}
+            $newDonations = $newDonations.concat(newItems);
+            donationsSeen = results.length;
+        }
+    }
 
-			$participantName.html( participant );
-			$donation.html( value );
-		}
-	}
+    function updateDonation() {
+        if ($newDonations.length > 0) {
+            let donationText;
+            let participant;
+            const curDonation = $newDonations.pop();
+
+            if (curDonation) {
+                const donorName = !curDonation.displayName ? ELT.settings.unknownDonorName : curDonation.displayName;
+
+                participant = $participants[curDonation.participantID].displayName;
+                const amount = curDonation.amount ? ELT.toCurrency(curDonation.amount) : ELT.settings.unknownDonationAmountText;
+                donationText = `<span class="donor-name">${donorName}</span><span class="donor-separator">:</span><span class="donor-amount">${amount}</span>`;
+            } else {
+                participant = ' ';
+                donationText = 'No donations';
+            }
+
+            $participantName.html(participant);
+            $donation.html(donationText);
+
+            if (ELT.settings.animate) {
+                $trackingContainer.addClass('animate-in');
+                setTimeout(() => {
+                    $trackingContainer.removeClass('animate-in');
+                }, ELT.settings.animationPauseMS);
+            }
+        }
+    }
 
     start();
 })(window.jQuery, window.ELT);
