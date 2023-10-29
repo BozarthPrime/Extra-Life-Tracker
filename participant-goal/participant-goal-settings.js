174ae04dfca98d5cfe3bb2c7481a77c6826c5805
Joseph Bozarth <bozarjp@gmail.com>
2023-10-29 11:24:58 -0500

    I25: Add progress bar to personal goal

--- participant-goal/participant-goal-settings.js
+++ participant-goal/participant-goal-settings.js
@@ -1,10 +1,12 @@
 ELT.settings = {
 	// get goal and amount raised a single participant
-	participantId: "297208",
+	participantId: "523738",
 	// if the header message should be shown
 	showHeader: true,
 	// header message to display at the top of the widget
 	headerMessage: "Goal",
 	// how often should data be refreshed
 	refreshTimeMS: 15000,
+	// Have the progress bar
+	progressBar: true
 };
